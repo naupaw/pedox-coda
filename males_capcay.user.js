ecf6624ffcb442aeb3b10a0cd407bf84f58f8993
Pedox <cungkring.ceking@gmail.com>
2011-12-27 21:34:04 +0700

    Update Fix bug

--- males_capcay.user.js
+++ males_capcay.user.js
@@ -3,38 +3,23 @@
 // @namespace      pedox
 // @description    buat yang males matematika
 // @include        http://www.kaskus.us/newreply.php?*
+// @include        http://www.kaskus.us/newthread.php?*
 // ==/UserScript==
 
 
-var $;
-
-// Add jQuery
-    (function(){
-        if (typeof unsafeWindow.jQuery == 'undefined') {
-            var GM_Head = document.getElementsByTagName('head')[0] || document.documentElement,
-                GM_JQ = document.createElement('script');
-    
-            GM_JQ.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
-            GM_JQ.type = 'text/javascript';
-            GM_JQ.async = true;
-    
-            GM_Head.insertBefore(GM_JQ, GM_Head.firstChild);
-        }
-        GM_wait();
-    })();
-
-// Check if jQuery's loaded
-    function GM_wait() {
-        if (typeof unsafeWindow.jQuery == 'undefined') {
-            window.setTimeout(GM_wait, 100);
-        } else {
-            $ = unsafeWindow.jQuery.noConflict(true);
-            letsJQuery();
-        }
-    }
+function addJQuery(callback) {
+  var script = document.createElement("script");
+  script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
+  script.addEventListener('load', function() {
+    var script = document.createElement("script");
+    script.textContent = "(" + callback.toString() + ")();";
+    document.body.appendChild(script);
+  }, false);
+  document.body.appendChild(script);
+}
 
 // All your GM code must be inside this function
-    function letsJQuery() {
+    function main() {
 		var capcay = $('label[for="humanverify"]').html();
 		var pemisa = capcay.split(" ");
 		
@@ -58,3 +43,4 @@ var $;
 		}
 		
     }
+addJQuery(main);
